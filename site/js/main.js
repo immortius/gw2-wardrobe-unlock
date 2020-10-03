fdb8cda5dc328fc51f6f5524a3483be42c62ce9a
Blackgen <jandb07@gmx.de>
2020-10-03 15:11:29 +0200

    Add trading post name to skins with differing name

--- site/js/main.js
+++ site/js/main.js
@@ -629,9 +629,9 @@ function calculateTotalValue(tpItems, priceFunc) {
 function getSellPrice(item) {
 	var itemValue = NaN;
 	if (item.priceData != null) {
-		itemValue = item.priceData.sellPrice;
+		itemValue = item.priceData.bestSellPrice.price;
 		if (isNaN(itemValue)) {
-			itemValue = item.priceData.buyPrice;
+			itemValue = item.priceData.bestBuyPrice.price;
 		}
 	}
 	var vendorPrice = getVendorPrice(item);
@@ -644,9 +644,9 @@ function getSellPrice(item) {
 function getBuyPrice(item) {
 	var itemValue = NaN;
 	if (item.priceData != null) {
-		itemValue = item.priceData.buyPrice;
+		itemValue = item.priceData.bestBuyPrice.price;
 		if (isNaN(itemValue)) {
-			itemValue = item.priceData.sellPrice;
+			itemValue = item.priceData.bestSellPrice.price;
 		}
 	}
 	var vendorPrice = getVendorPrice(item);
@@ -954,19 +954,29 @@ function showDetails(item, prefix) {
 	}
 	
 	if (item.priceData) {
-		if (item.priceData.buyPrice) {
-			$('#' + prefix + 'selection-buy-gold').text(Math.floor(item.priceData.buyPrice / 10000));
-			$('#' + prefix + 'selection-buy-silver').text(Math.floor(item.priceData.buyPrice / 100) % 100);
-			$('#' + prefix + 'selection-buy-copper').text(item.priceData.buyPrice % 100);
+		if (item.priceData.bestBuyPrice.price) {
+		  if(item.priceData.bestBuyPrice.itemName != item.name) {
+		    $('#' + prefix + 'selection-buy-name').text("(" + item.priceData.bestBuyPrice.itemName + ")");
+		  } else {
+		    $('#' + prefix + 'selection-buy-name').text("");
+		  }
+			$('#' + prefix + 'selection-buy-gold').text(Math.floor(item.priceData.bestBuyPrice.price / 10000));
+			$('#' + prefix + 'selection-buy-silver').text(Math.floor(item.priceData.bestBuyPrice.price / 100) % 100);
+			$('#' + prefix + 'selection-buy-copper').text(item.priceData.bestBuyPrice.price % 100);
 		} else {
 			$('#' + prefix + 'selection-buy-gold').text('0');
 			$('#' + prefix + 'selection-buy-silver').text('0');
 			$('#' + prefix + 'selection-buy-copper').text('0');
 		}
-		if (item.priceData.sellPrice) {
-			$('#' + prefix + 'selection-sell-gold').text(Math.floor(item.priceData.sellPrice / 10000));
-			$('#' + prefix + 'selection-sell-silver').text(Math.floor(item.priceData.sellPrice / 100) % 100);
-			$('#' + prefix + 'selection-sell-copper').text(item.priceData.sellPrice % 100);
+		if (item.priceData.bestSellPrice.price) {
+		   if(item.priceData.bestSellPrice.itemName != item.name) {
+		     $('#' + prefix + 'selection-sell-name').text("(" + item.priceData.bestSellPrice.itemName + ")");
+       } else {
+      	 $('#' + prefix + 'selection-sell-name').text("");
+       }
+			$('#' + prefix + 'selection-sell-gold').text(Math.floor(item.priceData.bestSellPrice.price / 10000));
+			$('#' + prefix + 'selection-sell-silver').text(Math.floor(item.priceData.bestSellPrice.price / 100) % 100);
+			$('#' + prefix + 'selection-sell-copper').text(item.priceData.bestSellPrice.price % 100);
 		} else {
 			$('#' + prefix + 'selection-sell-gold').text('-');
 			$('#' + prefix + 'selection-sell-silver').text('-');
--- src/main/java/au/net/immortius/wardrobe/site/entities/Price.java
+++
@@ -1,9 +0,0 @@
-package au.net.immortius.wardrobe.site.entities;
-
-/**
- * Trading post price information for an unlock
- */
-public class Price {
-    public Integer buyPrice;
-    public Integer sellPrice;
-}
