0e543b84402c2d6de9564582cab8830fb81cf53b
immortius <immortius@gmail.com>
2023-08-20 22:44:15 +1000

    Reviewed and tweaked groups:  - Changed top level group names for releases to have abbreviations at the end.  - Changed cultural "Series" to "Tier" in line with in-game nomenclature.  - Changed crafting group names to use crafting rank rather than series in line with in-game nomeclature.  - Removed elite specialzation series names as adequately covered by top level grouping.  - Removed other arbitrary series numbering  - Removed Cooperative/Competative/Festival qualifies from top level group names as obvious to users.  - Simplied some groups names.  - Conformed group names to a standard <source>[: <classifier>[ - <set name>]] pattern.  - Removed Gem Store from group names as covered by top level grouping.  - Raised Aspect Eye/Enchanged Dragon Crown/Pointed Ears as non-standalone groupings.  - Updated use of types in line with ingame type names.  - Removed use of Other when other qualifiers were available.  - Raised Djinn and Elite Specialization Mentor mini groups as non-standalone.  - Renamed Gem Store Series 1/etc mini groups to Set 1/2/3 in line with in-game nomeclature.  - Renamed Baby - Set 1/2 to Baby: Series 1/2 in line with in-game nomeclature.  - Renamed Super - Set 1/2/3/4 to Super: Series 1/2/3/4 in line with (most recent) in-game nomeclature (looks like originally it would have been World 1/2/etc, but)  - For cultural/regional tier 1 and 2 sets, moved them to just be named <region> Region: <set name> (the asura/sylvari situation makes it clear they not really cultural). Brought Ruins of Orr Region weapons into line with this too. Tier 3 cultural weapons are referred to as <race> Cultural: <set>  - Changed legendary "series" to "generation" in line with standard community nomenclature. Fixed error handling with new hero api caching to handle api down errors.

--- src/main/java/au/net/immortius/wardrobe/gw2api/ApiCacher.java
+++ src/main/java/au/net/immortius/wardrobe/gw2api/ApiCacher.java
@@ -113,33 +113,39 @@ public class ApiCacher {
 
     private int readGroupedSkins(String apiUrl, Path toPath, boolean allSupported, boolean stringIds) throws IOException {
         Files.createDirectories(toPath);
-        int downloadCounter = 0;
-        if (allSupported) {
-            logger.info("Updating cache for {}", toPath);
-            String json = client.target(apiUrl + "?ids=all").request().get(String.class);
-            JsonParser parser = new JsonParser();
-            JsonElement root = parser.parse(json);
-            JsonArray rootArray = root.getAsJsonArray();
-            for (JsonElement element : rootArray) {
-                JsonObject obj = element.getAsJsonObject();
-                String name = obj.get("name").getAsString();
-                for (JsonElement rawSkin : obj.get("skins").getAsJsonArray()) {
-                    JsonObject skin = rawSkin.getAsJsonObject();
-                    String id = skin.get("id").getAsString();
-                    skin.addProperty("type", "Hero");
-                    JsonObject details = new JsonObject();
-                    details.addProperty("type", name);
-                    skin.add("details", details);
-                    Path outPath = toPath.resolve(id + ".json");
-                    try (JsonWriter writer = new JsonWriter(Files.newBufferedWriter(outPath, Charsets.UTF_8))) {
-                        gson.toJson(skin, JsonElement.class, writer); } catch (IOException e) {
-                        logger.error("Failed to write file {}", outPath, e);
+        try {
+            int downloadCounter = 0;
+            if (allSupported) {
+                logger.info("Updating cache for {}", toPath);
+                String json = client.target(apiUrl + "?ids=all").request().get(String.class);
+                JsonParser parser = new JsonParser();
+                JsonElement root = parser.parse(json);
+                JsonArray rootArray = root.getAsJsonArray();
+                for (JsonElement element : rootArray) {
+                    JsonObject obj = element.getAsJsonObject();
+                    String name = obj.get("name").getAsString();
+                    for (JsonElement rawSkin : obj.get("skins").getAsJsonArray()) {
+                        JsonObject skin = rawSkin.getAsJsonObject();
+                        String id = skin.get("id").getAsString();
+                        skin.addProperty("type", "Hero");
+                        JsonObject details = new JsonObject();
+                        details.addProperty("type", name);
+                        skin.add("details", details);
+                        Path outPath = toPath.resolve(id + ".json");
+                        try (JsonWriter writer = new JsonWriter(Files.newBufferedWriter(outPath, Charsets.UTF_8))) {
+                            gson.toJson(skin, JsonElement.class, writer);
+                        } catch (IOException e) {
+                            logger.error("Failed to write file {}", outPath, e);
+                        }
                     }
+                    downloadCounter++;
                 }
-                downloadCounter++;
             }
+            return downloadCounter;
+        } catch (ServiceUnavailableException e) {
+            logger.info("API current disabled for {}", toPath.getFileName());
+            return 0;
         }
-        return downloadCounter;
     }
 
     /**
